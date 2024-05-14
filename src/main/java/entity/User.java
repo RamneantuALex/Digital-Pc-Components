7f1282b6299f08e542137c508c0d122a6a2f7825
VladCojo <vladvcojocaru@gmail.com>
2024-05-14 14:36:26 +0300

    am creat clasa user

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,77 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Component;
+@Entity
+@Table(name = "")
+public class User {
+    @Id
+    private int id;
+    private String nume;
+    private String prenume;
+    private String email;
+    private String parola;
+
+    public User(){
+
+    }
+    public User(String nume, String prenume, String email, String parola) {
+        this.nume = nume;
+        this.prenume = prenume;
+        this.email = email;
+        this.parola = parola;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getParola() {
+        return parola;
+    }
+
+    public void setParola(String parola) {
+        this.parola = parola;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nume='" + nume + '\'' +
+                ", prenume='" + prenume + '\'' +
+                ", email='" + email + '\'' +
+                ", parola='" + parola + '\'' +
+                '}';
+    }
+}
