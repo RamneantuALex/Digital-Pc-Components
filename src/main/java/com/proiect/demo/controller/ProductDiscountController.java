c5d5ae15350bb97cd37525ee4f9ebf2fd78b61f8
VladCojo <vladvcojocaru@gmail.com>
2024-05-26 14:35:48 +0300

    gata product discount controller

---
+++ src/main/java/com/proiect/demo/controller/ProductDiscountController.java
@@ -0,0 +1,48 @@
+package com.proiect.demo.controller;
+
+import com.proiect.demo.entity.ProductDiscount;
+import com.proiect.demo.repository.ProductDiscountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productDiscount")
+public class ProductDiscountController {
+    @Autowired
+    private ProductDiscountRepository productDiscountRepository;
+
+    @GetMapping("/all")
+    public List<ProductDiscount> getAllProductDiscounts() {
+        return productDiscountRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDiscount getProductDiscountById(@PathVariable int id) {
+        return productDiscountRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public ProductDiscount createProductDiscount(@RequestBody ProductDiscount productDiscount) {
+        return productDiscountRepository.save(productDiscount);
+    }
+
+    @PostMapping("/{id}")
+    public ProductDiscount updateProductDiscount(@PathVariable int id, @RequestBody ProductDiscount productDiscount) {
+        ProductDiscount existingProductDiscount = productDiscountRepository.findById(id).orElse(null);
+        if (existingProductDiscount != null) {
+            existingProductDiscount.setDiscount(productDiscount.getDiscount());
+            existingProductDiscount.setEndDate(productDiscount.getEndDate());
+            existingProductDiscount.setProductId(productDiscount.getProductId());
+            existingProductDiscount.setStartDate(productDiscount.getStartDate());
+            return productDiscountRepository.save(existingProductDiscount);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProductDiscount(@PathVariable int id) {
+        productDiscountRepository.deleteById(id);
+    }
+}
