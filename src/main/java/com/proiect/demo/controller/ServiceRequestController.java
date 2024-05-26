b0fa22d048819338b7c82c6978ab603935da0f8d
VladCojo <vladvcojocaru@gmail.com>
2024-05-26 15:17:36 +0300

    gata si cu user controller

--- src/main/java/com/proiect/demo/controller/ServiceRequestController.java
+++ src/main/java/com/proiect/demo/controller/ServiceRequestController.java
@@ -45,4 +45,6 @@ public class ServiceRequestController {
         serviceRequestRepository.deleteById(id);
     }
 
+
+
 }
---
+++ src/main/java/com/proiect/demo/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.proiect.demo.controller;
+
+import com.proiect.demo.entity.User;
+import com.proiect.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            existingUser.setFirstName(user.getFirstName());
+            existingUser.setLastName(user.getLastName());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setRole(user.getRole());
+            existingUser.setPassword(user.getPassword());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+}
